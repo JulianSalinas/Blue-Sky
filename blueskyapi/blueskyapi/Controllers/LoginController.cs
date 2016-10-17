ebdda104ee42fb0b94b0fd09de15aec87cb5439e
Julian Salinas <julian4001@outlook.com>
2016-10-16 20:44:37 -0600

    Primer Avance

---
+++ blueskyapi/blueskyapi/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using blueskyapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace blueskyapi.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        LinqToSqlDataContext linq = new LinqToSqlDataContext();
+
+        [HttpGet]
+        public Sesion LogIn (int carnet) {
+
+            List<Sesion> candidatos = new List<Sesion>();
+
+            var usuario_1 = (   from profesor in linq.PROFESOR
+                                where profesor.CARNET == carnet
+                                select profesor );
+
+            foreach (PROFESOR profesor in usuario_1) {
+                Sesion usuario = new Sesion() {
+                    Carnet = (int) profesor.CARNET,
+                    Contrasenha = profesor.CONTRASENHA,
+                    Correo = profesor.CORREO,
+                    Nombre = profesor.NOMBRE,
+                    Tipo = "Profesor"
+                };
+                candidatos.Add(usuario);
+            }
+
+            if(candidatos.Count == 0) {
+                var usuario_2 = ( from estudiante in linq.ESTUDIANTE
+                                  where estudiante.CARNET == carnet
+                                  select estudiante);
+
+                foreach (ESTUDIANTE estudiante in usuario_2 ) {
+                    Sesion usuario = new Sesion() {
+                        Carnet = (int) estudiante.CARNET,
+                        Contrasenha = estudiante.CONTRASENHA,
+                        Correo = estudiante.CORREO,
+                        Nombre = estudiante.NOMBRE,
+                        Tipo = "Estudiante"
+                    };
+                    candidatos.Add(usuario);
+                }
+            }
+            return candidatos.ElementAt(0);
+        }
+
+
+    }
+}
